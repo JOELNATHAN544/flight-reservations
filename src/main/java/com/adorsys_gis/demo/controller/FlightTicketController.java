3a99133a90b88e50c9bd5407408682578a09bf08
JOELNATHAN544 <wankojoelnathan@gmail.com>
2025-07-09 10:40:28 +0100

    Enhance FlightTicketController by adding CORS support for frontend integration

--- src/main/java/com/adorsys_gis/demo/controller/FlightTicketController.java
+++ src/main/java/com/adorsys_gis/demo/controller/FlightTicketController.java
@@ -1,15 +1,24 @@
 package com.adorsys_gis.demo.controller;
 
-import com.adorsys_gis.demo.model.FlightTicket;
-import com.adorsys_gis.demo.service.FlightTicketService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.adorsys_gis.demo.model.FlightTicket;
+import com.adorsys_gis.demo.service.FlightTicketService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/tickets")
+@CrossOrigin(origins = "http://localhost:8081")
 @RequiredArgsConstructor
 public class FlightTicketController {
     private final FlightTicketService service;
